afd1fe852fc0f75c8bc7054d560698db882c783b
Andres Cano <andrescaza246@hotmail.com>
2021-11-15 21:58:52 -0500

    Funcionalidad generar ventas en pdf integrada

--- src/app/components/dashboard/reportes/reportes.component.ts
+++ src/app/components/dashboard/reportes/reportes.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import jsPDF from 'jspdf';
+import  autoTable  from 'jspdf-autotable';
+import { UsuariosComponent } from '../usuarios/usuarios.component';
+import { ApiService } from '../../../services/api.service';
 
 @Component({
   selector: 'app-reportes',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _api:ApiService) { }
+
+  doc:any = new jsPDF();
+  loading:boolean = false;
 
   ngOnInit(): void {
   }
 
+
+  downloadPDF()
+  {
+    this.loading = true;
+    this.doc.text("VENTAS DE SCHOOL GYM", 10,10);
+    this._api.getSales().subscribe((response:any)=>
+    {
+      this.loading = false;
+      console.log(response.map((venta:any)=>[venta.client.id, venta.client.name, venta.client.lastName, venta.client.dateOfBirth,
+        venta.plan + ' ($' + venta.value + ')', venta.client.age, venta.client.email, venta.client.city,
+      venta.client.address, venta.membership.purchased_date]));
+      autoTable(this.doc,{
+        head: [['Cedula', 'Nombre', 'Apellido', 'fecha_nacimiento', 'Licencia','Edad', 'Email', 'Ciudad', 'Direccion', 'Fecha']],
+        body: response.map((venta:any)=>[venta.client.id, venta.client.name, venta.client.lastName, venta.client.dateOfBirth,
+          venta.plan + ' ($' + venta.value + ')', venta.client.age, venta.client.email, venta.client.city,
+        venta.client.address, venta.membership.purchased_date])
+      });
+
+      this.doc.save("ventas schoolgym.pdf");
+    })
+
+  }
 }
